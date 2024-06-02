8731d9e943b794148357b1086cd46e142f5efdfe
HarveyMendez <harvey.mendez@ucr.ac.cr>
2024-06-02 10:23:05 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Media, MediaObject } from '@awesome-cordova-plugins/media/ngx';
+import { File } from '@awesome-cordova-plugins/file/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Flashlight, Media, File, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Haptics, ImpactStyle } from '@capacitor/haptics'; //vibracion
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+import { Media, MediaObject } from '@awesome-cordova-plugins/media/ngx';
+import { Platform } from '@ionic/angular';
+import { File } from '@awesome-cordova-plugins/file/ngx';
+import { Network } from '@capacitor/network';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +14,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  flashlightOn: boolean = false;
+
+  filePath!: string; 
+  audio!: MediaObject; 
+  recording: boolean = false;
+
+  isConnected: boolean = false;
+
+  constructor(private flashlight: Flashlight, private media: Media, private file: File, private platform: Platform) {
+    this.platform.ready().then(() => {
+      if (this.platform.is('android')) {
+        this.filePath = this.file.externalDataDirectory + 'audio.mp3';
+      } else {
+        this.filePath = this.file.documentsDirectory + 'audio.mp3';
+      }
+    });
+  }
+
+  async vibrate() {
+    await Haptics.impact({ style: ImpactStyle.Heavy });
+  }
+
+
+  toggleFlashlight() {
+    if (this.flashlightOn) {
+      this.flashlight.switchOff();
+    } else {
+      this.flashlight.switchOn();
+    }
+    this.flashlightOn = !this.flashlightOn;
+  }
+
+  startRecord() {
+    this.audio = this.media.create(this.filePath);
+    this.audio.startRecord();
+    this.recording = true;
+  }
+
+  stopRecord() {
+    this.audio.stopRecord();
+    this.recording = false;
+  }
+
+  playAudio() {
+    this.audio = this.media.create(this.filePath);
+    this.audio.play();
+  }
+
+
+  async checkNetworkStatus() {
+    const status = await Network.getStatus();
+    this.isConnected = status.connected;
+  }
 
 }
